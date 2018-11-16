37502f60f05d7f935691faabfbd12ec367aca856
Hartzler <ftn0813@missouristate.edu>
2018-11-16 12:55:00 -0600

    Built out maya render functions

--- tools/maya.js
+++ tools/maya.js
@@ -1,8 +1,50 @@
-const splitIntoChunks = (frameList, chunkSize) {
+const { spawn } = require('child_process');
 
+const settings = (startF, endF, proj, outDir, format, cam, step=1, pad=000, resX=1920, resY=1080) => ({
+  startF,
+  endF,
+  proj,
+  outDir,
+  format,
+  cam,
+  step,
+  pad,
+  resX,
+  resY
+});
+
+const splitIntoChunks = (settings, chunkSize) {
+  let chunks = [];
+  for (let frame = settings.startF; frame <= settings.endF; frame+=chunkSize) {
+    let chunk = {
+      ...settings,
+      startF: frame,
+      endF: Math.min(frame + chunkSize - 1, settings.endF)
+    }
+    chunks.push(chunk);
+  }
+  return chunks;
 }
 
-const startRender = (startF, endF, step, proj, outDir, format, pad, cam, resX, resY) => {
-    `${process.env.MAYA_BIN_DIR}\\Render.exe -r file -s ${startF} -e ${endF} -b ${step} -proj ${proj} -rd ${outDir} -fnc name.ext -pad ${pad} -of ${format} -cam ${cam} -x ${resX} -y ${resY}`;
+const startRender = (chunk, cb) => {
+    const command = chunk => `${process.env.MAYA_BIN_DIR}\\Render.exe -r file -s ${chunk.startF} -e ${chunk.endF} -b ${chunk.step} -proj ${chunk.proj} -rd ${chunk.outDir} -fnc name.ext -pad ${chunk.pad} -of ${chunk.format} -cam ${chunk.cam} -x ${chunk.resX} -y ${chunk.resY}`;
+    const child = spawn(command(chunk));
+    child.stdout.on('data', info => {
+      cb(null, null, info);
+    });
+    child.stderr.on('data', errorInfo => {
+      cb(null, errorInfo, null);
+    });
+    child.on('error', error => {
+      cb.(error, null, null);
+    });
+    child.on('exit', (code, signal) => {
+      cb.(null, signal, code, true);
+    });
 }
-/**/
+
+module.exports = {
+  settings,
+  splitIntoChunks,
+  startRender
+};

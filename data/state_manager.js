2324ba51dab8cec7edc144f2baca080cda63422c
Hartzler <ftn0813@missouristate.edu>
2018-11-30 14:58:34 -0600

    Running DSCA Sync

--- data/state_manager.js
+++ data/state_manager.js
@@ -1,6 +1,8 @@
 const db = require('./local_db');
 const reducer = require('./reducer');
 const middleware = require('./middleware');
+const fs = require('fs');
+let stateChange = 1;
 let win;
 
 const getState = top_level_property => {
@@ -11,7 +13,8 @@ const getState = top_level_property => {
 const setState = newState => {
   db.setState(newState);
   db.write();
-  //state = newState;
+  fs.writeFileSync(`stateChange${stateChange}.txt`, JSON.stringify(newState));
+  stateChange++;
 }
 
 const dispatch = (action, fromPeer) => {
